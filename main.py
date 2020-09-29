bfa711e3e5a6210f9f349bb049c214e64d87073f
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2020-09-29 10:37:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 def measure(text):
-    test_string = str(text)
-    res = test_string.encode('utf-8')
-    return len(res) * 0.001
+    try:
+        test_string = str(text)
+        #change to utf-16 to get utf-16 length
+        res = test_string.encode('utf-8')
+        return len(res) * 0.001
+    except:
+        return 0
 
+    
 print(measure("hello"))
