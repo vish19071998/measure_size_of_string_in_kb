e82a6df087390184d2584c636cd6c577668f34d4
vishwak <49344980+vishwakneelamegam@users.noreply.github.com>
2020-09-29 11:08:32 +0530

    added encode type  argument

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-def measure(text):
+def measure(text,encType=1):
     try:
         test_string = str(text)
         #change to utf-16 to get utf-16 length
-        res = test_string.encode('utf-8')
+        if encType == 1:
+            res = test_string.encode('utf-8')
+        if encType == 2:
+            res = test_string.encode('utf-16')
         return len(res) * 0.001
     except:
         return 0
 
     
-print(measure("hello"))
+print(measure("hello",1))
